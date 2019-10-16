0daa35bb68e339296ca54450c53f61da8f85a1d4
Kelvin Forteta <kelvin.forteta@gmail.com>
2019-10-16 22:19:15 +0100

    Updates

---
+++ precache-manifest.754245f80bba92b4b38e56e068530ae2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80305073313460e5c61317d6848b86a",
+    "url": "/robotfriends-reactjs/index.html"
+  },
+  {
+    "revision": "0faa60d1d3d7d62255c7",
+    "url": "/robotfriends-reactjs/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9ce300e27efdf6c9841f",
+    "url": "/robotfriends-reactjs/static/css/main.f0c40f2f.chunk.css"
+  },
+  {
+    "revision": "0faa60d1d3d7d62255c7",
+    "url": "/robotfriends-reactjs/static/js/2.8d8e6903.chunk.js"
+  },
+  {
+    "revision": "9ce300e27efdf6c9841f",
+    "url": "/robotfriends-reactjs/static/js/main.1f5d4ba3.chunk.js"
+  },
+  {
+    "revision": "2dd0ee4fcb6a2f08b865",
+    "url": "/robotfriends-reactjs/static/js/runtime-main.12bc3df1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends-reactjs/static/media/SEGA.12a3db8b.woff"
+  }
+]);
